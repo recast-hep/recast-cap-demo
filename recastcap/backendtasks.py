c34c6e5c4298f41bd8b15308381af680668675e8
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-05-05 02:13:12 -0400

    waiting for full log

--- recastcap/backendtasks.py
+++ recastcap/backendtasks.py
@@ -22,7 +22,8 @@ def recast(ctx):
     
     if 'RECAST_IN_DOCKER_WORKDIRS_VOL' in os.environ:
         #publish absolute path of this workdir for use by plugins
-        yadage_env['PACKTIVITY_WORKDIR_LOCATION']='/'.join([os.environ['RECAST_IN_DOCKER_WORKDIRS_VOL'],workdir])
+        workdirpath = '/'.join([os.environ['RECAST_IN_DOCKER_WORKDIRS_VOL'],workdir])
+        yadage_env['PACKTIVITY_WORKDIR_LOCATION'] = '{}:{}'.format(os.path.abspath(workdir),workdirpath)
         log.info('plugin is running in Docker. set packtivity workdir as %s',yadage_env['PACKTIVITY_WORKDIR_LOCATION'])
           
     cmd = 'yadage-run -t {toplevel} {workdir} {workflow} {context}'.format(
@@ -47,19 +48,25 @@ def recast(ctx):
     filelogger.addHandler(fh)
     filelogger.propagate = False
     
-    while proc.poll() is None:
+    while True:
+        
         s = proc.stdout.readline().strip()
-        filelogger.debug(s)
-        try:
-            splitup = s.split(':',1)
-            if len(splitup)==2:
-                level,rest = splitup
-                thelevel = getattr(logging,level)
-                for line in rest.splitlines():
-                    if 'adage' in line or thelevel>logging.INFO:
-                        log.log(thelevel,rest)
-        except AttributeError:
-            pass
+        if s:
+            filelogger.debug(s)
+            try:
+                splitup = s.split(':',1)
+                if len(splitup)==2:
+                    level,rest = splitup
+                    thelevel = getattr(logging,level)
+                    for line in rest.splitlines():
+                        if 'adage' in line or thelevel>logging.INFO:
+                            log.log(thelevel,rest)
+            except AttributeError:
+                pass
+        else:
+            print 'here',proc.poll()
+            if proc.poll() is not None:
+                break
         time.sleep(0.01)
     
     log.info('workflow process terminated task')
