a5c51a30a9dac65b4e870b09e828d91ee28b369c
lukasheinrich <lukas.heinrich@gmail.com>
2017-02-07 22:56:07 +0100

    krb5 and stuff.. sorry giordon

--- recastyadage/tracker.py
+++ recastyadage/tracker.py
@@ -1,20 +1,26 @@
 import json
 import os
 import recastcelery.messaging
+import logging
 from yadage.helpers import WithJsonRefEncoder
-
+from adage.tracker import SimpleReportTracker
 class RECASTTracker(object):
     def __init__(self):
         self.jobguid = os.environ['RECAST_JOBGUID']
+        self.log, self.handler = recastcelery.messaging.setupLogging(self.jobguid)
+        self.tracker = SimpleReportTracker(self.log,30)
 
     def initialize(self,adageobj):
-        self.track(adageobj)
+    	self.tracker.initialize(adageobj)
+        # self.track(adageobj)
 
     def track(self,adageobj):
-        serialized = json.dumps(adageobj.json(), cls=WithJsonRefEncoder, sort_keys=True)
-        recastcelery.messaging.socketlog(self.jobguid,'noop tracking for now.. but still alive')
+    	self.tracker.track(adageobj)
+        # serialized = json.dumps(adageobj.json(), cls=WithJsonRefEncoder, sort_keys=True)
+        # recastcelery.messaging.socketlog(self.jobguid,'noop tracking for now.. but still alive')
         # recastcelery.messaging.generic_message(self.jobguid,'yadage_state',json.loads(serialized))
 
     def finalize(self,adageobj):
-        self.track(adageobj)
+    	self.tracker.finalize(adageobj)
+        # self.track(adageobj)
 
