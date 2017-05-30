914c10e75da12736081f555f6678231283b30125
lukasheinrich <lukas.heinrich@gmail.com>
2017-05-30 11:19:09 +0200

    add packtivity redis logger

---
+++ yadageworker/packredislogger.py
@@ -0,0 +1,27 @@
+import os
+import logging
+import redis
+import json
+
+class RedisHandler(logging.StreamHandler):
+    def __init__(self,jobid,topic):
+    	self.red = redis.StrictRedis(
+    		host = os.environ.get('PACKTIVITY_LOGGER_HOST','localhost'),
+    		port = os.environ.get('PACKTIVITY_LOGGER_HOST',6379),
+    		db = os.environ.get('PACKTIVITY_LOGGER_HOST',0)
+    	)
+    	self.jobid = jobid
+    	self.topic = topic
+        logging.StreamHandler.__init__(self)
+
+    def emit(self, record):
+    	msg = self.format(record)
+    	data = {'jobguid': self.jobid, 'msg_type':'simple_log', 'msg':msg, 'topic': self.topic}
+    	self.red.publish(os.environ.get('PACKTIVITY_LOGGER_CHANNEL','logstash:in'),json.dumps(data))
+
+formatter = logging.Formatter('%(message)s')
+def add_handlers(log,nametag,context,topic):
+    fh = RedisHandler(nametag,topic)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
