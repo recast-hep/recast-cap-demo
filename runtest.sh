12054245412363d4eccb825bff7773f64c3f6a3f
lukasheinrich <lukas.heinrich@gmail.com>
2017-08-08 11:01:19 +0200

    remove historical remnants

--- runtest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf workdirs
-mkdir -p workdirs/jobguid/inputs
-(cd workdirs/jobguid/inputs && curl -O http://physics.nyu.edu/~lh1132/michal.zip && unzip michal.zip)
-./testrun.py pheno_workflows/madgraph_delphes.yml jobguid
