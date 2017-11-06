0d55b4bb66bea00d9de930f2014963b2323e61d3
lukasheinrich <lukas.heinrich@gmail.com>
2017-11-06 15:17:58 +0100

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'wflow-yadage-worker',
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'wflow-yadage-worker',
   url = '',
   author = 'Lukas Heinrich',
